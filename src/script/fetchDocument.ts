b7de57bc643d08d2a4a58e9db3f49d80c8899220
mina-lue <min_alu@outlook.com>
2025-11-11 11:28:54 +0300

    headless

--- src/script/fetchDocument.ts
+++ src/script/fetchDocument.ts
@@ -3,7 +3,7 @@ import { solveRecaptcha } from "./utils/solveRecaptcha";
 import { pay } from "./utils/pay";
 
 export const fetchDocument = async (ACN : string ) : Promise<string> => {
-  const browser = await chromium.launch({ headless: false }); 
+  const browser = await chromium.launch({ headless: true }); 
   const context = await browser.newContext();
   const page = await context.newPage();
 
